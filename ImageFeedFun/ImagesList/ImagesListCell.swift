34f7d7f51756b736e896d9448ece95a409393c7d
ZakharVP <Freelander2015en>
2024-09-04 00:14:43 +0300

    Выполнены задачи в рамках блока "Спринт 08"

---
+++ ImageFeedFun/ImagesList/ImagesListCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImagesListCell.swift
+//  ImageFeedFun
+//
+//  Created by Захар Панченко on 01.09.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet private var contentCell: UIView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateTextLAbel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
