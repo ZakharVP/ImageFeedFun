7340f6b107ba5db7a8d092f26243b15f97f18804
ZakharVP <Freelander2015en>
2024-09-04 23:50:33 +0300

    Оптимизирована конструкция quard

--- ImageFeedFun/ImagesList/ImagesListViewController.swift
+++ ImageFeedFun/ImagesList/ImagesListViewController.swift
@@ -53,8 +53,9 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-        guard let imageListCell = cell as? ImagesListCell else {
+        guard let imageListCell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier,
+            for: indexPath) as? ImagesListCell else {
             return UITableViewCell()
         }
         configCell(for: imageListCell, with: indexPath)
