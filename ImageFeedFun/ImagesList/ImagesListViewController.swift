19f44c52ac142981b31d18ab673cc803fe6df895
ZakharVP <Freelander2015en>
2024-09-04 23:37:45 +0300

    Добавлен комментарий в пустую функцию

--- ImageFeedFun/ImagesList/ImagesListViewController.swift
+++ ImageFeedFun/ImagesList/ImagesListViewController.swift
@@ -29,7 +29,9 @@ final class ImagesListViewController: UIViewController {
 
 extension ImagesListViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { 
+        // TODO: - Добавить логику при нажатии на ячейку
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
